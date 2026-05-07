48fde2be888c0ad0c85915930900f5a2a03bce0f
kumar Atul munesh <atulmuneshdev@gmail.com>
2026-05-07 13:07:14 +0530

    Change API base URL to production server

--- clint/src/api/axios.js
+++ clint/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
     // baseURL: 'http://127.0.0.1:3420/api',
-    baseURL: 'http://127.0.0.1:3420/api',
+    baseURL: 'https://justice-portal.onrender.com/api',
     
 
     withCredentials: true
